8c464772ca4ad1c99dec60e7cc146268db746c71
Kelley van Evert <hello@kelleyvanevert.nl>
2019-10-24 22:15:06 +0200

    step 4 completed (like/dislike)

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,11 +1,33 @@
 import React from "react";
 
 export default class Quote extends React.Component {
+  state = {
+    liked: null
+  };
+
+  setLiked = () => {
+    this.setState({ liked: "yes" });
+  };
+
+  setNotLiked = () => {
+    this.setState({ liked: "no" });
+  };
+
   render() {
+    let style = {};
+    if (this.state.liked === "yes") {
+      style = { fontWeight: "bold", color: "green" };
+    } else if (this.state.liked === "no") {
+      style = { textDecoration: "line-through", color: "red" };
+    }
+
     return (
       <div>
-        <p>{this.props.quote}</p>
-        <p>By: {this.props.author}</p>
+        <p style={style}>{this.props.quote}</p>
+        <p>
+          By: {this.props.author} <button onClick={this.setNotLiked}>:(</button>{" "}
+          <button onClick={this.setLiked}>:)</button>
+        </p>
       </div>
     );
   }
